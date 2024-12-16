60659d5be0edd25d5ab2313f9c667f2b4f1c6816
Jay.D <djccnt15@gmail.com>
2024-12-17 08:29:09 +0900

    create PageList component for left nav bar

--- src/component/PageList.jsx
+++ src/component/PageList.jsx
@@ -1,15 +1,23 @@
+import styles from './css/PageList.module.css'
 import { NavLink } from 'react-router-dom'
 
 const navItems = [
   { name: 'Home', path: '/' },
+  { name: 'Binary Threshold', path: '/binary-threshold' },
+  { name: 'Get Coordinate', path: '/get-coordinate' },
 ]
 
 const PageList = () => {
   return (
-    <ul>
+    <ul className={styles.ul}>
       {navItems.map((item) => (
         <li key={item.path}>
-          <NavLink to={item.path}>{item.name}</NavLink>
+          <NavLink
+            to={item.path}
+            className={({ isActive }) => (isActive ? styles.activeLink : styles.link)}
+          >
+            {item.name}
+          </NavLink>
         </li>
       ))}
     </ul>
