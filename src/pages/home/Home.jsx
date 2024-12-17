9c2ca098cfddceb097878813dd7af8571ae4ca50
Jay.D <djccnt15@gmail.com>
2024-12-17 12:58:56 +0900

    update Home page apply css module

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
-import PageList from "../component/PageList"
+import styles from "./Home.module.css"
+import PageList from "../../component/PageList"
 
 const Home = () => {
   return (
-    <>
-      <div>비전 시스템 개발용 보조 기능 모음</div>
+    <div className={styles.home}>
+      <h1>비전 시스템 개발용 보조 기능 모음</h1>
       <PageList />
-    </>
+    </div>
   )
 }
 
